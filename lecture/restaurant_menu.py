0caeaa4b26bd7eaff27fb7bfc36f8fabca81e803
Youngbeen-Noh <noyoungbin40@gmail.com>
2024-11-06 13:04:55 +0900

    week10 end

--- lecture/restaurant_menu.py
+++ lecture/restaurant_menu.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -8,19 +8,34 @@ app = Flask(__name__)
 def showRestaurants(name=None):
     db = sqlite3.connect('restaurant_menu.db')
     cursor = db.cursor()
-    items = cursor.execute('SELECT name FROM restaurant').fetchall()
+    items = cursor.execute('SELECT id, name FROM restaurant').fetchall()
     db.close()
-    # print(items)
-    mydoc = "<h1>All Restaurants</h1>"
-    mydoc += "<ul>"
-    for item in items:
-        mydoc += f"<li>{item[0]}</li>"
-    mydoc += "</ul>"
-    return mydoc
 
-@app.route('/restaurant/new/')
+    # 디버깅용 print
+    print(items)
+
+    # python에서 html짜기
+    # mydoc = "<h1>All Restaurants</h1>"
+    # mydoc += "<ul>"
+    # for item in items:
+    #     mydoc += f"<li>{item[0]}</li>"
+    # mydoc += "</ul>"
+    return render_template('restaurants.html', restaurants=items)
+
+@app.route('/restaurant/<int:restaurant_id>/')
+def showMenu(restaurant_id):
+    return f"All menu items for restaurant {restaurant_id}"
+
+@app.route('/restaurant/new/', methods=['GET', 'POST'])
 def newRestaurant():
-    return "This page will be for making a new restaurant"
+    if request.method == 'POST':
+        db = sqlite3.connect('restaurant_menu.db')
+        cursor = db.cursor()
+        cursor.execute('INSERT INTO restaurant (name) VALUES (?)', (request.form['name'],))
+        db.commit()
+        db.close()
+        return redirect('/restaurants/')
+    return render_template('restaurant_new.html')
 
 @app.route('/restaurant/<int:restaurant_id>/delete/')
 def deleteRestaurant(restaurant_id):
